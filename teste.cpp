b72f1a850651710c779db8b3642be0b6fe9d2c83
hgleo <hleomelo1@gmail.com>
2023-05-03 17:01:15 -0300

    commit

--- teste.cpp
+++ teste.cpp
@@ -1,12 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(){
 
-  cout << "Hello World" << endl;
+  vector<int> vet;
 
+   for(int i = 0; i < 40;i++){
+
+    vet.push_back(i);
+
+    cout << "Size: " << vet.size() <<" || Element: "<< vet[i]<< " || Capacity: "<< vet.capacity()<< endl;
+
+   }
+   vet.shrink_to_fit();
+   cout << "Capacity: " << vet.capacity()<< endl;
+   
+   vet.erase(vet.begin()+7);
+
+  for(int i = 0; i < vet.size(); i++){
+    cout <<" Position: " << i <<" || Value: " << vet[i] << endl;
+  }
   return 0;
 }
